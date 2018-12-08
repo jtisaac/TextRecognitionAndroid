670221f9b4538500f2d763e26896f37420befb0d
jtisaac <jtisaac@users.noreply.github.com>
2018-12-08 16:59:58 -0600

    Save File button works.

--- app/src/main/java/com/tuts/prakash/simpleocr/MainActivity.java
+++ app/src/main/java/com/tuts/prakash/simpleocr/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 import android.widget.Toast;
@@ -24,7 +27,11 @@ import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,24 +40,84 @@ public class MainActivity extends AppCompatActivity {
     CameraSource mCameraSource;
 
     private static final String TAG = "MainActivity";
-    private static final int requestPermissionID = 101;
+    private static final int requestPermissionIDcamera = 101;
+    private static final int requestPermissionIDfile = 1000;
+    private String texttosave = "No Text Yet";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestPermissionIDfile);
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        requestPermissionIDfile);
+            }
+        } catch (Exception e) {
+            Context context = getApplicationContext();
+            Toast toast = Toast.makeText(context, "file error", Toast.LENGTH_LONG);
+            toast.show();
+            e.printStackTrace();
+        }
         mCameraView = findViewById(R.id.surfaceView);
         mTextView = findViewById(R.id.text_view);
         final Button saveButton = findViewById(R.id.SaveText);
         saveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Context context = getApplicationContext();
+                /*try {
+
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                requestPermissionIDfile);
+                        Toast savetoast = Toast.makeText(context, "returning", Toast.LENGTH_LONG);
+                        savetoast.show();
+                        return;
+                    }
+                    //mCameraSource.start(mCameraView.getHolder());
+                } catch (Exception e) {
+                    Toast permissionsToast = Toast.makeText(context, e.toString(), Toast.LENGTH_LONG);
+                    permissionsToast.show();
+                    e.printStackTrace();
+                }*/
+
                 CharSequence text = "Saving";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
                 Log.d("SaveButton was clicked", "savebutton");
+                // Thanks to https://stackoverflow.com/questions/28755934/
+                // android-studio-writing-text-to-a-file-using-a-save-button-in-the-menu for snippet
+                String filename = "OCR_Text.txt";
+                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), filename);
+                Toast filer = Toast.makeText(context, "test" + getFileStreamPath(filename).toString(), duration);
+                //filer.show();
+
+                try {
+                    FileOutputStream output = new FileOutputStream(file);
+                    //OutputStreamWriter output = new OutputStreamWriter(openFileOutput("TextReaderFile", MODE_APPEND));
+                    //EditText ET = (EditText)findViewById(R.id.editText);
+                    //String text = ET.getText().toString();
+                    output.write(texttosave.getBytes());
+                    output.close();
+                    Toast fileToast = Toast.makeText(context, "The contents are saved in the file.", duration);
+                    fileToast.show();
+                } catch (FileNotFoundException e) {
+                    //Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+                    Toast toaster = Toast.makeText(context, e.toString(), duration);
+                    toaster.show();
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Toast toasting = Toast.makeText(context, e.toString(), duration);
+                    toasting.show();
+                    e.printStackTrace();
+                }
             }
         });
         final Button homeScreen = findViewById(R.id.HomeScreen);
@@ -74,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != requestPermissionID) {
+        if (requestCode != requestPermissionIDcamera || requestCode != requestPermissionIDfile) {
             Log.d(TAG, "Got unexpected permission result: " + requestCode);
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             return;
@@ -123,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
 
                             ActivityCompat.requestPermissions(MainActivity.this,
                                     new String[]{Manifest.permission.CAMERA},
-                                    requestPermissionID);
+                                    requestPermissionIDcamera);
                             return;
                         }
                         mCameraSource.start(mCameraView.getHolder());
@@ -167,6 +234,7 @@ public class MainActivity extends AppCompatActivity {
                                     stringBuilder.append("\n");
                                 }
                                 mTextView.setText(stringBuilder.toString());
+                                texttosave = stringBuilder.toString();
                             }
                         });
                     }
